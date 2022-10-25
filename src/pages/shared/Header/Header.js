f8787bfd7863589b076f5a05747319f5a1760971
aroy15 <anjonroy57@gmail.com>
2022-10-25 15:53:26 +0600

    header design completed

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -1,12 +1,63 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image'
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { FaMoon, FaSun } from "react-icons/fa";
+
+
 
 const Header = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
+    const [themes, setThemes] = useState(true);
+    const handleThemes = () => setThemes(!themes);
+
     return (
-        <div>
-            {user.displayName}
-        </div>
+        <Navbar bg="light" expand="lg" className='shadow-sm'>
+            <Container className=''>
+                <Navbar.Brand href="#home">Programming Education</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#home">Courses</Nav.Link>
+                        <Nav.Link href="#link">FAQ</Nav.Link>
+                        <Nav.Link href="#link">Blog</Nav.Link>
+                    </Nav>
+                    <div className='d-flex gap-2 align-items-center'>
+                        <div>
+                            <a className='text-decoration-none' href="#">Login</a>
+                            <OverlayTrigger
+                                placement='left'
+                                overlay={
+                                    <Tooltip id='tooltip-bottom2'>
+                                        Tooltip on.
+                                    </Tooltip>
+                                }
+                            >
+                                <Button variant="link">
+                                    <Image roundedCircle src="https://lh3.googleusercontent.com/ogw/AOh-ky2K-6YfaeRXmwx67nuO3xkD0QvMf3JBZ9mV3ri5oA=s32-c-mo" alt="" />
+                                </Button>
+                            </OverlayTrigger>
+                        </div>
+                        <div>
+                            <Button onClick={handleThemes} variant="link" className='text-dark text-decoration-none'>
+                                {
+                                    themes ?
+                                        <><FaMoon /> Dark</>
+                                        :
+                                        <><FaSun /> Light</>
+                                }
+                            </Button>
+                        </div>
+                    </div>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
