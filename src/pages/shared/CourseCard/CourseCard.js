e0186c71c16621636f433b8d113540ff1d1f033e
aroy15 <anjonroy57@gmail.com>
2022-10-27 20:34:07 +0600

    card design updated and favicon added

--- src/pages/shared/CourseCard/CourseCard.js
+++ src/pages/shared/CourseCard/CourseCard.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import { FaCheck } from "react-icons/fa";
 
 const CourseCard = ({ course }) => {
-    const { course_title, _id, details_overview, image_url, overview_title, ratings } = course
+    const { course_title, _id, details_overview, image_url } = course
     const detailsOverview = details_overview.slice(0, 3);
+
+    const CourseCardItemList = ({detail}) =>{
+        return(
+            <p className='d-flex align-items-center gap-2 mb-2'>
+                    <span>
+                        <FaCheck className='text-success'/>
+                    </span>
+                    <span
+                        className='text-dark d-block text-decoration-none fw-semibold'
+                    >{detail}</span>
+            </p>
+        )
+    }
+
     return (
         <Card className='shadow border-success'>
             <Card.Img variant="top" className='card_img p-3' src={image_url} />
             <Card.Body>
                 <Card.Title>{course_title}</Card.Title>
-                <div className='card-text'>
-                    <ul>
+                <div className='card-text py-4'>
+                   
                         {
-                            detailsOverview.map((detail, index) => <li
-                                key={index}
-                            >{detail}</li>)
+                            detailsOverview.map((detail, index) => <CourseCardItemList key={index} detail={detail}/>)
                         }
-                    </ul>
                 </div>
                 <Link to={`/courses/${_id}`} className='btn btn-success'>Reade More</Link>
             </Card.Body>
