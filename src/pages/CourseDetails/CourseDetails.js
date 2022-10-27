0a12768c9832098a9706ebe2dcfc703e061836fe
aroy15 <anjonroy57@gmail.com>
2022-10-27 19:56:15 +0600

    readme file updated

--- src/pages/CourseDetails/CourseDetails.js
+++ src/pages/CourseDetails/CourseDetails.js
@@ -3,41 +3,50 @@ import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './CourseDetails.css';
-import { FaGoogle, FaGithub, FaPrint } from "react-icons/fa";
-import { useReactToPrint } from 'react-to-print';
+import { FaGoogle, FaGithub, FaPrint, FaDownload } from "react-icons/fa";
+import Pdf from "react-to-pdf";
+// import { useReactToPrint } from 'react-to-print';
 
+const ref = React.createRef();
 
 const CourseDetails = () => {
     const { course_title, details_overview, image_url, _id, overview_title, ratings } = useLoaderData()
 
-    const printingAreaRef = useRef()
-    const handlePrint = useReactToPrint({
-        content: () => printingAreaRef.current,
-        documentTitle:'emp-data',
-        onAfterPrint:()=>alert('Print Success')
-    })
+    // const printingAreaRef = useRef()
+    // const handlePrint = useReactToPrint({
+    //     content: () => printingAreaRef.current,
+    //     documentTitle:'emp-data',
+    //     onAfterPrint:()=>alert('Print Success')
+    // })
 
     return (
-        <Card className='card_details' ref={printingAreaRef}>
-            <Card.Header className='d-flex justify-content-between align-items-center gap-3'>                
-                <h5 className="py-0 my-0">Courses Full Overview</h5>
-                <Button onClick={handlePrint} variant='link' className="text-decoration-none text-success"><FaPrint/> Print</Button>
-            </Card.Header>
-            <Card.Img variant="top" className='p-3 card_img rounded-0' src={image_url} />
-            <Card.Body>
-                <h2 className='card-title mb-3'>{course_title}</h2>
-                <div className='card-text'>
-                    <ul>
-                        {
-                            details_overview.map((detail, index) => <li
-                                key={index}
-                            >{detail}</li>)
-                        }
-                    </ul>
-                </div>
-                <Link to={`/checkout/${_id}`} className="btn btn-success">Get premium access</Link>
-            </Card.Body>
-        </Card>
+        <>
+            <Card className='card_details' >
+                <Card.Header className='d-flex justify-content-between align-items-center gap-3'>
+                    <h5 className="py-0 my-0">Courses Full Overview</h5>
+                    <Pdf targetRef={ref} filename="course-details.pdf">
+                        {({ toPdf }) => <Button onClick={toPdf} variant='link' className="text-decoration-none text-success"><FaDownload /> Download PDF</Button>}
+                    </Pdf>
+                    
+                </Card.Header>
+                <Card.Img variant="top" className='p-3 card_img rounded-0' src={image_url} />
+                <Card.Body>
+                    <div ref={ref} style={{padding:'10px',width:'100%'}}>
+                        <h2 className='card-title mb-3'>{course_title}</h2>
+                        <div className='card-text'>
+                            <ul>
+                                {
+                                    details_overview.map((detail, index) => <li
+                                        key={index}
+                                    >{detail}</li>)
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    <Link to={`/checkout/${_id}`} className="btn btn-success">Get premium access</Link>
+                </Card.Body>
+            </Card>
+        </>
     );
 };
 
