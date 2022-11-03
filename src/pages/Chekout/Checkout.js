85c3e1f3553e86257fd49c29fc1743ec7edafbcb
aroy15 <anjonroy57@gmail.com>
2022-11-03 13:50:06 +0600

    checkout page updated

--- src/pages/Chekout/Checkout.js
+++ src/pages/Chekout/Checkout.js
@@ -3,18 +3,14 @@ import { useLoaderData } from 'react-router-dom';
 import Card from 'react-bootstrap/Card';
 
 const Checkout = () => {
-    const { course_title, details_overview, image_url } = useLoaderData()
+    const { course_title, image_url } = useLoaderData()
     return (
         <Card className="bg-dark text-dark">
             <Card.Img className='p-3' src={image_url} alt={course_title} />
             <Card.ImgOverlay className="d-flex flex-column align-items-center justify-content-center">
-                <div className='card-text p-3 rounded shadow-lg w-100' style={{backgroundColor:'rgba(194,211,244,0.90)',maxWidth:'500px'}}>                    
+                <div className='card-text p-3 rounded shadow-lg w-100 text-center' style={{backgroundColor:'rgba(194,211,244,0.90)',maxWidth:'500px'}}>                    
                     <Card.Title>{course_title}</Card.Title>
-                    <ul>
-                        {
-                            details_overview.map((detail, index) =><li key={index}>{detail}</li>)
-                        }
-                    </ul>
+                    <h4 className='text-success'>Thanks for booking</h4>
                 </div>
             </Card.ImgOverlay>
         </Card>
